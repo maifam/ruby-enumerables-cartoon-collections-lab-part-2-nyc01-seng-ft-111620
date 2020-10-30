5fb3d13932d41648e6b7910a5202df6ba53ffeb1
Mai Pham <maitphm@gmail.com>
2020-10-30 04:47:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,6 +32,8 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
-  planeteer_calls.any? { |call| call = valid_calls}
-  
+  planeteer_calls.find do |item|
+    valid_calls.include?(item)
+  end 
+  return item 
 end 
